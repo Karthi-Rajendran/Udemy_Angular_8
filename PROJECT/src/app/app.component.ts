5e63013a8c7eaa3d5a9f4da6ae95014b4259b938
Karthi_Rajendran <karthikrajan06@gmail.com>
2020-01-13 19:09:59 +0530

    Auth commit

--- PROJECT/src/app/app.component.ts
+++ PROJECT/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'PROJECT';
 
   // before observables: Subject operator
@@ -13,6 +14,15 @@ export class AppComponent {
   // onNavigate(feature: string){
   //   this.loadedFeature = feature;
   // }
+
+  constructor(private authService: AuthService){ }
+
+  
+  ngOnInit(){
+    this.authService.autoLogIn();
+  }
+
+
 
 
 }
--- PROJECT/src/app/header/header.component.ts
+++ PROJECT/src/app/header/header.component.ts
@@ -1,14 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',    
 })
-export class HeaderComponent{   
+export class HeaderComponent implements OnInit, OnDestroy{   
     collapsed = true;
+
+    isAuthenticated = false;
+    private userSub: Subscription;
+    router: any;
      // before Routing 
   // @Output() featureSelected = new EventEmitter<string>();
    
@@ -19,7 +25,17 @@ export class HeaderComponent{
 
     //HTTP section
 
-    constructor(private dataStorageService: DataStorageService){}
+    constructor(private dataStorageService: DataStorageService,private authService: AuthService){}
+
+    ngOnInit(){
+        this.userSub = this.authService.user.subscribe(
+            user => {
+                  //  this.isAuthenticated = !user ? false : true;
+                     this.isAuthenticated = !!user;
+                     console.log(!user);
+                     console.log(!!user);              
+                });
+    }
 
     onSaveData(){
         this.dataStorageService.storeRecipes();
@@ -27,5 +43,13 @@ export class HeaderComponent{
 
     onFetchData(){
         this.dataStorageService.fetchRecipes().subscribe();
+    }
+
+    onLogout(){
+       this.authService.logout(); 
+    }
+
+    ngOnDestroy(){
+        this.userSub.unsubscribe();
     }
 }
